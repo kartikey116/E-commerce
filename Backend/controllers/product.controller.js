04b2fcfb7058d91b1e4b337cf082108442405dac
KARTIKEY <kartikeyu07@gmail.com>
2025-08-10 23:24:20 +0530

    add new

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -1,6 +1,15 @@
 import Product from "../models/product.model.js";
+import redis from "../config/redis.config.js";
 
-export const getAllProducts = async (req, res) => {}
+export const getAllProducts = async (req, res) => {
+	try {
+		const products = await Product.find({});
+		res.json({ products });
+	} catch (error) {
+		console.log("Error in getAllProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+}
 
 export const getFeaturedProducts = async (req, res) => {
     try {
@@ -26,5 +35,37 @@ export const getFeaturedProducts = async (req, res) => {
 	} catch (error) {
 		console.log("Error in getFeaturedProducts controller", error.message);
 		res.status(500).json({ message: "Server error", error: error.message });
+	}
+}
+
+export const createProduct = async (req, res) => {
+	try {
+		const {name, description, price, image, category} = req.body;
+		let cloudinaryResponse = null;
+
+		if (image) {
+			cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" });
+		}
+
+		const product = await Product.create({
+			name,
+			description,
+			price,
+			image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+			category,
+		});
+
+		res.status(201).json(product);
+	} catch (error) {
+		console.log("Error in createProduct controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+
+export const deleteProduct = async (req, res) => {
+	try {
+		
+	} catch (error) {
+		
 	}
 }
--- Backend/routes/product.routes.js
+++ Backend/routes/product.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import {getAllProducts , getFeaturedProducts} from "../controllers/product.controller.js";
+import {getAllProducts , getFeaturedProducts, createProduct, deleteProduct} from "../controllers/product.controller.js";
 import { adminRoutes, protectRoutes } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/",protectRoutes,adminRoutes,getAllProducts);
 router.get("/featured",getFeaturedProducts);
+router.get("/recommendation",getRecommendation);
+router.post("/",protectRoutes,adminRoutes,createProduct);
+router.delete("/:id",protectRoutes,adminRoutes,deleteProduct);
 
 export default router;
